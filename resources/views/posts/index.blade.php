d867b6b0f1a2436d4f3827ba0c7ac5510ee3e54e
meushi <taku73baseball@gmail.com>
2024-02-18 11:53:45 +0900

    記事投稿作成画面

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang ="{{ str_replace('_', '-', app ()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="http://fonts.googleapis.com/css?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    
+    </head>
+    <body class= "antialiased">
+        <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginete'>{{ $posts->links()}}</div>
+    </body>
+</html>
